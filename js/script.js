a81666c1381c95e80c8a2cca5130de9861e4cc8c
Sandy <sangzicao@gmail.com>
2019-01-22 12:30:51 -0800

    added ability to pick 12 random cards & shuffle deck

--- js/script.js
+++ js/script.js
@@ -1,34 +1,63 @@
 var cards = document.querySelectorAll(".card-container");
 var images = document.getElementsByTagName('img');
-var cardFontSides = document.querySelectorAll('.card-front-side');
+var cardFrontSides = document.querySelectorAll('.card-front-side');
 
-console.log('hey it worked!', cardFontSides[0].src);
+console.log('hey it worked!', cardFrontSides[0].src);
+
 // Stored images
-//       let imageBank = [
-//         {arya: }
-//       ]
+      let imageBank = [
+        'images/arya.jpg',
+        'images/brienne.jpg',
+        'images/bronn.jpg',
+        'images/cersei.jpg',
+        'images/daenerys.jpg',
+        'images/hodor.jpg',
+        'images/jaime.jpg',
+        'images/jaqen.png',
+        'images/jon.png',
+        'images/podrick.jpg',
+        'images/sansa.jpg',
+        'images/tyrion.jpg'
+      ]
 
-// Pick 12 random ones from the bank
-
+// Pick 12 random cards from image bank
+      let pick12images = function (arr) {
+        let oldArr = arr.slice();
+        let newArr = [];
+        for (let i=0; i<12; i++) {
+          let randomPosition = Math.floor(Math.random() * oldArr.length)
+          newArr.push(oldArr[randomPosition])
+          oldArr.splice(randomPosition,1)
+        }
+        return newArr
+      }
+      let images12 = pick12images(imageBank)
 // Images x2
-//       let imageBankDouble = [...imageBank,...imageBank];
+      let imagesDoubled = [...imageBank,...imageBank];
 
-// Randomly assigning images functionality
-//       let shuffleCards = function () {
-
-//       }
-
-// Shuffled images 
-//       var shuffledDeck = shuffleCards(imageBankDouble);
-
-// Assigning cards to the images in order
-      for (let i=0; i<=cardFrontSides.length; i++) {
-        cardFrontSides[1].src = shuffledDeck[i]
+// Shuffle cards functionality
+      let shuffleCards = function (arr) {
+        let newArr = [];
+        let oldArr = arr.slice();
+        for (let i=0; i<cardFrontSides.length;i++) {
+          let randomPosition = Math.floor(Math.random() * oldArr.length)
+          newArr.push(oldArr[randomPosition])
+          oldArr.splice(randomPosition,1)
+        }
+        return newArr;
       }
 
+      // More efficient way to do this with the Fisher-Yates shuffle or Knuth
+          // let pick12Random = function (cards, n) {
+          //   let newCards = [];
 
+//Shuffled images 
+      let imagesShuffled = shuffleCards(imagesDoubled);
 
-
+// Assigning cards to the images in order
+      for (let i=0; i<cardFrontSides.length; i++) {
+        cardFrontSides[i].src = imagesShuffled[i]
+      }
 
 // Flipping the cards
 // cards.addEventListener('click', function (event) {
