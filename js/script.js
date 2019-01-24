1640133fb6255df0aa3ce075c70cc8c0bfb6705c
Sandy <sangzicao@gmail.com>
2019-01-24 14:58:48 -0800

    added score tracking, added ability to unflip cards, added ability to disable clicks during 3 second wait

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-var cardGrid = document.getElementById("card-grid");
-var cards = document.querySelectorAll('.card-container');
-var images = document.getElementsByTagName('img');
-var cardFrontSides = document.querySelectorAll('.card-front-side');
-var cardBackSides = document.querySelectorAll('.card-back-side')
+const cardGrid = document.getElementById("card-grid");
+const cards = document.querySelectorAll('.card-container');
+const images = document.getElementsByTagName('img');
+const cardFrontSides = document.querySelectorAll('.card-front-side');
+const cardBackSides = document.querySelectorAll('.card-back-side');
+const scoreBoard = document.getElementById('score');
+
+/* -----------------SETUP CARDS LOGIC-----------------------*/
 
 // Give all cards the same background image
 for (let i=0; i<cardBackSides.length; i++) {
@@ -92,68 +95,123 @@ for (let i=0; i<cardBackSides.length; i++) {
         cardFrontSides[i].src = imagesShuffled[i]
       }
 
-      
+/* -----------------GAME LOGIC-----------------------*/
 
-// GLOBAL VARIABLES
+
+// 1. GLOBAL VARIABLES
         
-    // Declare variable to capture the lastFlipped card
-          let lastFlipped = ''
+let firstCardImg = ''
+let firstCardClassList = ''
+let secondCardImg = ''
+let secondCardClassList = ''
+let score = 0
     
-    // Declare hasFlipped function for later
-          let hasFlipped = function(arr){
-            for (let i=0; i<arr.length; i++) {
-              if (arr[i].childNodes[1].className === 'flipped') {
-                return true
-              }
-            }
-            return false
-          }
+// 2. Declare hasFlipped function
+const hasFlipped = function(arr){
+  for (let i=0; i<arr.length; i++) {
+    if (firstCardImg === null) {
+      return true
+    }
+  }
+  return false
+}
+// 3. Declare flipCard function 
+const flipCard = function() {
+  event.target.parentElement.classList.toggle('flip');
+}
+
+// 4. Flip cards back function
+const unflipCards = function() {
+  secondCardClassList.remove('flipped');
+  firstCardClassList.remove('flipped');
+  cardGrid.addEventListener('click', memoryGame);
+}
+
+// 5. Update score function
+const updateScore = function() {
+  scoreBoard.innerHTML = "Score: " + score;
+}
+
+// 6. Disable click event function
+const disableCards = function() {
+  cardGrid.removeEventListener('click', memoryGame);
+}
+
+// 7. Reset board function
+
 
 // Event Listener when you click on any of the cards
-      cardGrid.addEventListener('click', function (event) {
-        
-        // Find the image that we just we just clicked on
-          let currentImage = event.target.parentNode.childNodes[3].src;
+cardGrid.addEventListener('click', memoryGame)
 
-        // Step 1: Was a flipped card clicked?
+function memoryGame(event) {
 
-          if (event.target.className === "card-front-side") {
-            // if true, do nothing
-            console.log('I did nothing')
-            return
-          } 
+  // Find the image that we just we just clicked on
+  let currentImage = event.target.parentNode.childNodes[1].src;
+  let currentClass = event.target.className;
+  let currentClassList = event.target.classList;
 
-        // if false, do any cards have the class of "flipped"?
+  // Step 1: Did we click on an already flipped card?
+  if (currentClass === "card-front-side") {
 
-            else if (!hasFlipped(cards)) {
+  // If true, then do nothing
+  console.log('I did nothing')
+    return
+  } 
 
-              // if no flipped cards found, then set current class to flipped
-              // and also set the value of currentImage to the current image
+  // If false, then check to see if a card has already been flipped
 
-                  event.target.className = 'flipped'
-                  lastFlipped = currentImage
-                  console.log('card was set to flipped and lastFlipped = ', lastFlipped)
+  else if (firstCardImg === '') {
 
-               // if a card has been flipped, then are the images the same?
-               // has a match been found?
+  // if no flipped cards found, then we know this is the first card to be flipped
+  // 1. record firstCardImg and firstCardClassList 
+  // 2. set the currentClass to 'flipped'
+  // 3. do the flip animation
 
-                  } else if (lastFlipped === currentImage) {
+    flipCard();
+    currentClassList.add('flipped');
+    firstCardImg = currentImage;
+    firstCardClassList = currentClassList;
+    score ++;
+    updateScore();
 
-                    // if a match has been found then set the clicked card to be flipped
-                      console.log("match found!")    
-                      event.target.className = 'flipped'
-                    // **Do a little animation here to tell the user they got it right**
+    console.log('card was set to flipped and firstCardImg = ', currentImage)
 
-                    // if no match was found:
-                    //  1. keep both cards facing up for 3 seconds
-                    //  2. nothing is allowed to be clicked
-                    //  3. flip cards over after 3 seconds
+  // if a card has already been flipped, then is there a match?
 
-                  } else {
-                    console.log("match was not found!")
-                  }
-                });
+  } else {
 
-// Known bugs:
-// 1. for some reason last else statement is not reached if a match is not found
-//     same thing for if a match is found
+    secondCardImg = currentImage;
+    secondCardClassList = currentClassList;
+    
+    if (firstCardImg === currentImage) {
+
+      // Match was found!
+      // 1. set the current card to be flipped
+      // 2. reset firstCardImg to ''
+      // 3. ***update score
+      // 4. ***do animation here to let the user know they got it right
+      console.log("match found!")    
+      flipCard();
+      currentClassList.add('flipped');
+      firstCardImg = '';
+      score ++;
+      updateScore();
+  
+    } else {
+
+      // Match was not found!
+      //  1. keep both cards facing up for 3 seconds
+      //  2. disable event listener
+      //  3. flip cards over after 3 seconds
+      console.log("match was not found!")
+      disableCards();
+      currentClassList.add('flipped');
+      flipCard();
+      setTimeout(unflipCards, 2000);
+      score ++;
+      updateScore();
+      firstCardImg = '';
+    }
+  }
+};
+
